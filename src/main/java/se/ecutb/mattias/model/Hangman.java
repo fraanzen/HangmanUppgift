2a806d486f62f2ca78a2c334e969c234f5defe05
fraanzen <mattiasfranzen_@hotmail.com>
2019-09-24 15:12:39 +0200

    Hangman completed.

---
+++ src/main/java/se/ecutb/mattias/model/Hangman.java
@@ -0,0 +1,83 @@
+package se.ecutb.mattias.model;
+
+import java.util.Arrays;
+
+public class Hangman{
+
+    private int numberOfGuessesMade;
+    public final int MAXGUESSES = 8;
+    private StringBuilder guessedLetters;
+    private String secretWord;
+    private char[] wordToGuess;
+
+    public Hangman(String secretWord){
+        numberOfGuessesMade = 0;
+        this.guessedLetters = new StringBuilder();
+        this.secretWord = secretWord;
+        this.wordToGuess = new char[secretWord.length()];
+        Arrays.fill(wordToGuess, '_');
+
+    }
+
+    public boolean theGuess(String wordGuess){
+        if (wordGuess.equalsIgnoreCase(secretWord)){
+            wordToGuess = secretWord.toCharArray();
+            return true;
+        }
+        numberOfGuessesMade++;
+        return false;
+    }
+
+    public boolean theGuess(char letterGuess){
+        if (secretWord.contains(letterGuess + "")){
+            for (int i = 0; i < wordToGuess.length ; i++){
+                if (letterGuess == secretWord.charAt(i)){
+                    wordToGuess[i] = letterGuess;
+                }
+            }
+            guessedLetters.append(wordToGuess);
+            return true;
+        }
+        numberOfGuessesMade++;
+        guessedLetters.append(letterGuess);
+        return false;
+    }
+
+    public boolean checkGuess(String input){
+        input = input.toLowerCase();
+        if (input.length() > 1){
+            return theGuess(input);
+        }if (!guessedLetters.toString().contains(input)){
+            return theGuess(input.charAt(0));
+        }
+        return false;
+    }
+
+    public boolean checkWin(){
+        boolean winner = String.valueOf(wordToGuess).equals(secretWord);
+        return winner;
+    }
+
+    public int getGuessCount() {
+        return this.numberOfGuessesMade;
+    }
+
+    public String getGuessedLetters() {
+        return guessedLetters.toString();
+    }
+    @Override
+    public String toString(){
+        return String.copyValueOf(wordToGuess);
+    }
+}
+
+
+/*    public boolean theGuess(char letterGuess){
+        if (secretWord.contains(letterGuess + "")){
+            for (int i = 0; i < wordToGuess.length ; i++){
+                if (letterGuess == wordToGuess[i]){
+                    wordToGuess[i] = letterGuess;
+                    guessedLetters.append(letterGuess);
+                }
+                }
+ */
---
+++ src/test/java/se/ecutb/mattias/AppTest.java
@@ -0,0 +1,64 @@
+package se.ecutb.mattias;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import se.ecutb.mattias.model.Hangman;
+
+public class AppTest{
+
+    Hangman game;
+
+    @Before
+    public void init(){
+        game = new Hangman("test");
+    }
+
+    @Test
+    public void test_of_init(){
+        String expected = "____";
+        String actual = game.toString();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_if_word_contains_guess(){
+        String letterGuess = "t";
+        Assert.assertTrue(game.checkGuess(letterGuess));
+        String expectedResult = "t__t";
+        Assert.assertEquals(expectedResult, game.toString());
+        int expectedGuessCount = 0;
+        Assert.assertEquals(expectedGuessCount, game.getGuessCount());
+    }
+
+    @Test
+    public void correct_full_word_guess(){
+        String wordGuess = "test";
+        Assert.assertTrue(game.checkGuess(wordGuess));
+        Assert.assertEquals(wordGuess, game.toString());
+        Assert.assertTrue(game.checkWin());
+    }
+
+    @Test
+    public void wrong_guess_test(){
+        String letterGuess = "f";
+        Assert.assertFalse(game.checkGuess(letterGuess));
+        String expectedResult = "____";
+        Assert.assertEquals(expectedResult, game.toString());
+        int expectedGuessCount = 1;
+        Assert.assertEquals(expectedGuessCount, game.getGuessCount());
+    }
+
+     @Test
+    public void wrong_full_word_guess_test(){
+        String wrongWordGuess = "fail";
+        Assert.assertFalse(game.checkGuess(wrongWordGuess));
+        int expectedGuessCount = 1;
+        Assert.assertEquals(expectedGuessCount, game.getGuessCount());
+        Assert.assertFalse(game.checkWin());
+
+     }
+
+}
